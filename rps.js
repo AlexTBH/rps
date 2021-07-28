bf766725d7744d6362a79c44a7a10321b8307339
Alexander Hoang <Aleexx.hoang@gmail.com>
2021-07-28 20:31:29 +0200

    Added a function for 1 round of RPS

--- rps.js
+++ rps.js
@@ -1,25 +1,39 @@
-function computerSelection() {
+function computerPlay() {
 
-    let computerChoice = Math.floor((Math.random()*3) + 1);
-
-        if (computerChoice == 1) {
-            console.log("Rock");
-        } else if (computerChoice == 2) {
-            console.log("Paper");
+    let computerMath = Math.floor((Math.random()*3) + 1);
+    let computerChoice = "";
+        if (computerMath == 1) {
+            computerChoice = "r";
+        } else if (computerMath == 2) {
+            computerChoice = "p";
         } else {
-            console.log("Scissor");
-    }
+            computerChoice = "s";
+        }
+
+    return computerChoice;
 }
 
 function playRound() {
-    let playerSelection = prompt("Choose between Rock, Paper & Scissor");
+    let playerSelection = prompt("Choose between Rock, Paper & Scissor").toLowerCase();
+    let computerSelection = computerPlay();
 
-    console.log(playerSelection.toLowerCase);
+    switch(computerSelection + playerSelection) {
+        case "rscissor":
+        case "prock":
+        case "spaper":
+            console.log("Computer wins!");
+        break;
+
+        case "rpaper":
+        case "pscissor":
+        case "srock":
+            console.log("You win!");
+        break;
+        
+        case "rrock":
+        case "ppaper":
+        case "sscissor":
+            console.log("It/'s a tie!");
+        break;
+    }
 }
-
-// Trying to make players answers case insensitive.
-
-
-
-
-
