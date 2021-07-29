b022fdc672dd032afc3e9bdec56f25a69b3efb66
Alexander Hoang <Aleexx.hoang@gmail.com>
2021-07-29 20:14:05 +0200

    Made a function for a round of 5 games, product finished

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function computerPlay() {
 
     let computerMath = Math.floor((Math.random()*3) + 1);
@@ -17,17 +20,20 @@ function playRound() {
     let playerSelection = prompt("Choose between Rock, Paper & Scissor").toLowerCase();
     let computerSelection = computerPlay();
 
+
     switch(computerSelection + playerSelection) {
         case "rscissor":
         case "prock":
         case "spaper":
             console.log("Computer wins!");
+            computerScore++;
         break;
 
         case "rpaper":
         case "pscissor":
         case "srock":
             console.log("You win!");
+            humanScore++;
         break;
         
         case "rrock":
@@ -36,4 +42,26 @@ function playRound() {
             console.log("It/'s a tie!");
         break;
     }
+
 }
+
+function game() {
+
+        for (i=0; i<5; i++) {
+            playRound();
+
+            console.log("The score is: ")
+            console.log("Computer: " + computerScore);
+            console.log("Human: " + humanScore);
+        }
+
+    if (computerScore > humanScore) {
+        console.log("Computer wins the game!");
+    } else if (computerScore == humanScore) {
+        console.log("Nobody won this game, it/'s a tie!")
+    } else {
+        console.log("Human wins the game!");
+    }
+}
+
+game();
